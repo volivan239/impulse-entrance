65c75cd8becf86252fca5d54370ad0e5a60aa7e3
Ivan Volkov <volkov.ivan2004@gmail.com>
2024-05-18 19:20:08 +0300

    Add file input

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include "base.h"
 #include "event.h"
 #include "club.h"
 
-std::pair<Club, std::vector<Event>> readInput() {
+std::pair<Club, std::vector<Event>> readInput(std::ifstream &fin) {
     int nTables;
     MyTime openingTime, closingTime;
     int hourPrice;
@@ -12,21 +13,21 @@ std::pair<Club, std::vector<Event>> readInput() {
     std::stringstream ss1, ss2, ss3;
     std::string line1, line2, line3;
 
-    getline(std::cin, line1);
+    getline(fin, line1);
     ss1 << line1;
     if (!(ss1 >> nTables)) {
         std::cerr << "Parsing error at line 1: " << line1 << std::endl;
         exit(1);
     }
 
-    getline(std::cin, line2);
+    getline(fin, line2);
     ss2 << line2;
     if (!(ss2 >> openingTime >> closingTime)) {
         std::cerr << "Parsing error at line 2: " << line2 << std::endl;
         exit(1);
     }
 
-    getline(std::cin, line3);
+    getline(fin, line3);
     ss3 << line3;
     if (!(ss3 >> hourPrice)) {
         std::cerr << "Parsing error at line 3: " << line3 << std::endl;
@@ -40,7 +41,7 @@ std::pair<Club, std::vector<Event>> readInput() {
     std::string eventStr;
     int lineNumber = 3;
     std::vector<Event> events;
-    while (getline(std::cin, eventStr)) {
+    while (getline(fin, eventStr)) {
         ++lineNumber;
         std::stringstream ss;
         ss << eventStr;
@@ -56,8 +57,14 @@ std::pair<Club, std::vector<Event>> readInput() {
     return {club, events};
 }
 
-int main() {
-    auto [club, events] = readInput();
+int main(int argc, const char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Expected excatly one argument: input file name" << std::endl;
+        return -1;
+    }
+    std::ifstream fin(argv[1]);
+
+    auto [club, events] = readInput(fin);
     std::cout << club.getOpeningTime() << '\n';
 
     for (const auto &event : events) {
